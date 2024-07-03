7b5969cda580fc00919f97ae2b3dff5b76ec432c
Kevin Muriuki Karimi <kevimuri24@gmail.com>
2024-07-03 19:17:09 +0300

    Finished Lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway"
+}
+
+function updateEmployeeWithKeyAndValue(employee, name, streetAddress){
+    const newEmployee = {...employee}
+    newEmployee.name = "Sam"
+    newEmployee.streetAddress = "11 Broadway"
+    return newEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+    employee.streetAddress = "12 Broadway"
+    return employee
+}
+
+function deleteFromEmployeeByKey(){
+    const newEmployee = {...employee}
+    delete newEmployee.name
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(){
+    delete employee.name
+    return employee
+}
